26335a1e8ba5f6a9b9fe192398bb09650618e901
Marco Brandizi <marco.brandizi@gmail.com>
2020-06-09 16:27:41 +0100

    Fixing git tagging under Travis. [ci skip]

--- travis/travis.sh
+++ travis/travis.sh
@@ -34,7 +34,7 @@ if [[ ! -z "$is_release" ]]; then
 	echo -e "\n\n\tCommitting ${NEW_RELEASE_VER} to github\n"
 	msg="Releasing ${NEW_RELEASE_VER}.${ci_skip_tag}"
 	git commit -a -m "$msg"
-	# For some reason, in Travis it results already tagged tat this point.
+	# For some reason, in Travis it results already tagged at this point.
 	git tag --force --annotate "${NEW_RELEASE_VER}" -m "$msg"
 	
 	echo -e "\n\n\tSwitching codebase version to ${NEW_SNAPSHOT_VER}\n"
@@ -57,7 +57,8 @@ if [[ ! -z "$needs_push" ]]; then
 	git_url=`echo "$git_url" | sed -E "s|http(s?)://|http\1://$GIT_USER:$GIT_PASSWORD@|"`
 	git remote set-url origin https://$GIT_USER:$GIT_PASSWORD@github.com/Rothamsted/knetminer-common.git
 
-	git push --tags origin HEAD:"$TRAVIS_BRANCH"
+	# It seems that Travis auto-pushes tags
+	git push --force --tags origin HEAD:"$TRAVIS_BRANCH"
 fi
 
 echo -e "\n\nThe End."
