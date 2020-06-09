c0ccabf7808492e93fc42100015a36b70f0a6535
Marco Brandizi <marco.brandizi@gmail.com>
2020-06-09 13:15:47 +0100

    Adding releasing operations to Travis scritps.

---
+++ travis/install.sh
@@ -0,0 +1,12 @@
+
+# This is only used by the default travis in the archetype-built projects. The main project
+# ignores it. 
+#
+cd `dirname "$0"`
+
+# Travis has to both define TRAVIS_DIR_URL and download me.
+#
+for file in maven-settings.xml travis.sh 
+do
+	wget "$TRAVIS_DIR_URL/master/travis/$file" -O "$file"
+done
---
+++ travis/travis.sh
@@ -0,0 +1,61 @@
+set -e
+cd `dirname "$0"`
+cd ..
+
+ci_skip_tag=' [ci skip]'
+
+# Manage releasing too, when these vars are defined
+#
+if [[ ! -z "${NEW_RELEASE_VER}" ]] && [[ ! -z "${NEW_SNAPSHOT_VER}" ]]; then 
+  echo -e "\n\n\tRELEASSING ${NEW_RELEASE_VER}, new snapshot will be: ${NEW_SNAPSHOT_VER}\n" 
+  is_release='true'
+fi
+  
+if [[ ! -z "$is_release" ]]; then
+  mvn versions:set -DnewVersion="${NEW_RELEASE_VER}" -DallowSnapshots=true
+  # Commit immediately, even if it fails, we will have a chance to give up
+  mvn versions:commit
+fi
+
+mvn deploy --settings "travis/maven-settings.xml"
+
+if git diff-index --quiet HEAD; then
+	needs_push='true'
+	git commit -a -m 'Updating Travis auto-generated files [ci skip]';
+fi
+
+# Will git need to be updated on remote?
+[[ ! -z "$is_release" || ! -z "$needs_push" ]] && needs_push='true'
+
+
+# And now manage the release too
+#
+if [[ ! -z "$is_release" ]]; then
+	echo -e "\n\n\tCommitting ${NEW_RELEASE_VER} to github\n"
+	msg="Releasing ${NEW_RELEASE_VER}.${ci_skip_tag}"
+	git commit -m "$msg"
+	git tag --annotate "${NEW_RELEASE_VER}" -m "$msg"
+	
+	echo -e "\n\n\tSwitching codebase version to ${NEW_SNAPSHOT_VER}\n"
+	mvn versions:set -DnewVersion="${NEW_SNAPSHOT_VER}" -DallowSnapshots=true
+	mvn versions:commit
+	git commit -m "Switching version to ${NEW_SNAPSHOT_VER}.${ci_skip_tag}"
+fi
+
+# Do we need to git-push?
+#
+if [[ ! -z "$needs_push" ]]; then
+	echo -e "\n\n\tPushing changes to github\n"
+	
+	git_url=`git remote -v | awk '$NR=2 {print $2; exit}'`
+	if [[ "$git_url" =~ ^https?://.+ ]]; then
+		echo "ERROR: No remote git URL or unsupported format (only http(s):// supported)."
+		exit 1
+	fi
+	
+	git_url=`echo "$git_url" | sed -E "s|http(s?)://|http\1://$GIT_USER:$GIT_PASSWORD@'"`
+	git remote set-url origin https://$GIT_USER:$GIT_PASSWORD@github.com/Rothamsted/knetminer-common.git
+	git push --tags origin HEAD:"$TRAVIS_BRANCH";
+fi
+
+echo -e "\n\nThe End."
