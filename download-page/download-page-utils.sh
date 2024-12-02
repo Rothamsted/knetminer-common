4bd71445c9c2dfe628ccf4221757dc3da82e4475
Marco Brandizi <marco.brandizi@gmail.com>
2024-12-02 17:53:31 +0000

    download page utils, updating Nexus URL. [ci skip]

--- download-page/download-page-utils.sh
+++ download-page/download-page-utils.sh
@@ -46,7 +46,7 @@ function nexus_asset_search
 	ext="$5"
 	version="$6" # This is the only one that is optional, last versions are fetched if omitted
 	
-	url="https://knetminer.com/artifactory"
+	url="https://knetminer.rothamsted.ac.uk/artifactory"
 	url="$url/service/rest/v1/search/assets?sort=version&direction=desc"
 	
 	url="$url&repository=$repo"
